9d099e81b4e79e85c400cda4ce244eb4d6948731
Robert Oost <robert.oost@mcmain.nl>
2026-06-05 11:03:14 +0200

    feat(content): add June check

--- content.js
+++ content.js
@@ -4,6 +4,11 @@ function mcPride() {
   
   if (host !== "localhost" && host !== "127.0.0.1") return;
   
+  const today = new Date();
+  const isJune = today.getMonth() === 5;
+
+  if (!isJune) return;
+
   const logoUrl = browser.runtime.getURL("mcpride.png");
   
   function replaceLogos() {
